0fece94c193abf896b3c0cd82873aa5da100476f
anitaorstad <anita.orstad@gmail.com>
2021-01-04 21:57:12 +0100

    Updated calculator

--- calculate.js
+++ calculate.js
@@ -4,12 +4,24 @@ function display(val) {
 }
 
 function solve() {
-    console.log("solve");
     let x = document.getElementById("result").value;
     let y = eval(x);
-    document.getElementById("result").value = y;
+    if (typeof y == "undefined") {
+        document.getElementById("result").value = "";
+    } else {
+        document.getElementById("result").value = y;
+    }
+
 }
 
 function clr() {
     document.getElementById("result").value = "";
+}
+
+function pi() {
+    document.getElementById("result").value = Math.PI;
+}
+
+function e() {
+    document.getElementById("result").value = Math.E;
 }
