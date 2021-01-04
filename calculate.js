609c836229f96863e222220307c089436308f1bb
anitaorstad <anita.orstad@gmail.com>
2021-01-04 16:58:29 +0100

    Created a calculator

---
+++ calculate.js
@@ -0,0 +1,15 @@
+
+function display(val) {
+    document.getElementById("result").value += val;
+}
+
+function solve() {
+    console.log("solve");
+    let x = document.getElementById("result").value;
+    let y = eval(x);
+    document.getElementById("result").value = y;
+}
+
+function clr() {
+    document.getElementById("result").value = "";
+}
--- myapp.java
+++ myapp.java
@@ -3,7 +3,7 @@ import javax.print.event.PrintEvent;
 public class myapp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!);
+        System.out.println("Hello World!");
 
     }
 }
